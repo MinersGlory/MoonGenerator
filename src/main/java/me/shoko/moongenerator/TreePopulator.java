c2c8ca259b6012848b7fb767f9a657535b3a3c67
Zethrus <contact@zethr.us>
2019-01-31 21:40:16 -0700

    Debugging and a chance system like FloraPopulator

--- src/main/java/me/shoko/moongenerator/TreePopulator.java
+++ src/main/java/me/shoko/moongenerator/TreePopulator.java
@@ -9,22 +9,32 @@ import java.io.File;
 import java.util.Random;
 
 public class TreePopulator extends BlockPopulator {
+    private static final int SMALL_TREE_CHANCE = 4; // 4 out of 100 chance
+
+
+    int amount;
 
     @Override
-    public void populate(World world, Random random, Chunk chunk) {
+    public void populate(World world, Random random, Chunk source) {
 
         //TODO: Make string dynamic and add all schem files in /schematics/trees to a for each (?)
         String schem = "moon_mushroom";
         File file = new File(MoonGenerator.getPlugin().getDataFolder() + File.separator + "/schematics/" + schem);
 
         if (random.nextBoolean()) {
-            int amount = random.nextInt(4)+1; // Amount of trees
+            amount = random.nextInt(4) + 1; // Amount of trees
             for (int i = 1; i < amount; i++) {
                 int X = random.nextInt(15);
                 int Z = random.nextInt(15);
                 int Y = 1;
-                for (int j = world.getMaxHeight()-1; chunk.getBlock(X, j, Z).getType() == Material.AIR; j--); // Find the highest block of the (X,Z) coordinate chosen.
-                SchemUtil.loadSchem(chunk.getBlock(X, Y, Z).getLocation(), schem);
+                for (int j = world.getMaxHeight()-1; source.getBlock(X, j, Z).getType() == Material.AIR; j--) Y = j; // Find the highest block of the (X,Z) coordinate chosen.
+                int chance = random.nextInt(100);
+                if (chance > 34) {
+                    SchemUtil.loadSchem(source.getBlock(X, Y, Z).getLocation(), schem);
+
+                    // For debugging to see if this even triggers 
+                    MoonGenerator.getPlugin().getLogger().info("A SCHEM HAS BEEN SPAWNED");
+                }
             }
         }
 
