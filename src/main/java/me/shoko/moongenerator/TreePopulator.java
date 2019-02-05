7acf7299cae273f60cb93510109cb5c083118251
Zethrus <contact@zethr.us>
2019-02-05 07:48:39 -0700

    Put within if statements

--- src/main/java/me/shoko/moongenerator/TreePopulator.java
+++ src/main/java/me/shoko/moongenerator/TreePopulator.java
@@ -35,16 +35,21 @@ public class TreePopulator extends BlockPopulator {
                 //int Y;
                 int Y = world.getHighestBlockYAt(X,Z);
                 for(int i2 = -1; i2<Y+1; i2++){
-                    Block b = world.getBlockAt(X, Y+i2, Z);
-                    if (source.getBlock(X, Y, Z).getType() == Material.DEAD_BRAIN_CORAL_BLOCK) {
+                    // set chance out of 100
+                    int chance = random.nextInt(100);
+                    //Block b = world.getBlockAt(X, Y+i2, Z);
+                    if (source.getBlock(X, Y+i2, Z).getType() == Material.DEAD_BRAIN_CORAL_BLOCK) {
 
-                        int chance = random.nextInt(100);
+
                         if (chance > 97) {
                             SchemUtil.loadSchem(source.getBlock(X, Y, Z).getLocation(),moonshroom);
 
                             // DEBUG
                             //MoonGenerator.getPlugin().getLogger().info("SPAWNED A MOONSHROOM");
                         }
+                    }
+
+                    if (source.getBlock(X, Y+i2, Z).getType() == Material.DEAD_BRAIN_CORAL_BLOCK) {
                         if (chance  > 86) {
                             SchemUtil.loadSchem(source.getBlock(X, Y, Z).getLocation(),moontreelarge);
 
@@ -52,6 +57,9 @@ public class TreePopulator extends BlockPopulator {
                             // DEBUG
                             //MoonGenerator.getPlugin().getLogger().info("SPAWNED A LARGE MOON TREE");
                         }
+                    }
+
+                    if (source.getBlock(X, Y+i2, Z).getType() == Material.DEAD_BRAIN_CORAL_BLOCK) {
                         if (chance > 64) {
                             SchemUtil.loadSchem(source.getBlock(X, Y, Z).getLocation(),moontreemedium);
 
@@ -59,6 +67,10 @@ public class TreePopulator extends BlockPopulator {
                             // DEBUG
                             //MoonGenerator.getPlugin().getLogger().info("SPAWNED A MEDIUM MOON TREE");
                         }
+                    }
+
+                    if (source.getBlock(X, Y + i2, Z).getType() == Material.DEAD_BRAIN_CORAL_BLOCK) {
+
                         if (chance > 54) {
                             SchemUtil.loadSchem(source.getBlock(X, Y, Z).getLocation(),moontreesmall);
 
@@ -70,6 +82,7 @@ public class TreePopulator extends BlockPopulator {
                             //MoonGenerator.getPlugin().getLogger().info("SPAWNED A SMALL MOON TREE");
                         }
                     }
+
                 }
 
                 // TODO: Correctly 0-255 getBlock() scan for highest source block
